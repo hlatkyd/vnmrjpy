a4e92e3496800d7e8d0582c5af6303719ee09849
Hlatky David <hlatkydavid@gmail.com>
2019-04-21 18:33:55 +0200

    pipy dist first test

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setuptools.setup(
     version='0.1.dev1',
     author='David Hlatky',
     author_email='hlatkydavid@gmail.com',
-    url='https://github.com/hlatkydavid/vnmrjpy'
-    description='Handle VnmrJ MRI data and recostruction with Python'
+    url='https://github.com/hlatkydavid/vnmrjpy',
+    description='Handle VnmrJ MRI data and recostruction with Python',
     long_description=long_description,
     python_requires='>=3.5',
     packages=setuptools.find_packages(exclude=['dataset','dataset*','vnmrsys']),
