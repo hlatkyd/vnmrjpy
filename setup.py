c2a20b6c087ab019fbca555bc46ed10796274a2f
Hlatky David <hlatkydavid@gmail.com>
2019-04-25 10:02:51 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-import setuptools
 import glob
+from setuptools import setup, find_packages
 
 with open('README.md','r') as fh:
     long_description = fh.read()
 
 script_list = glob.glob('vnmrjpy/scripts/*')
 
-setuptools.setup(
+setup(
     name='vnmrjpy',
-    version='0.1.dev2',
+    version='0.1.dev3',
     author='David Hlatky',
     author_email='hlatkydavid@gmail.com',
     url='https://github.com/hlatkydavid/vnmrjpy',
@@ -18,7 +18,7 @@ setuptools.setup(
     #packages=setuptools.find_packages(exclude=\
     #['dataset','test','.test.*','*.test.*',\
     #'vnmrsys','vnmrjpy.bin','vnmrjpy.recon','vnmrjpy.io']),
-    packages=['vnmrjpy'],
+    packages=find_packages(exclude=['test']),
     include_package_data=True,
     scripts=script_list,
     install_requires=[
