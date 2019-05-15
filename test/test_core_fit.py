666b9b5aaf38b5efe81a7e3f1c98dab22ffdabb6
Hlatky David <hlatkydavid@gmail.com>
2019-05-15 21:35:29 +0200

    fit3d utils started

--- test/test_core_fit.py
+++ test/test_core_fit.py
@@ -2,27 +2,42 @@ import unittest
 import vnmrjpy as vj
 import numpy as np
 import matplotlib.pyplot as plt
-from lmfit import minimize, Parameters
+import lmfit
+from vnmrjpy.core import make_params3d, fit3d
+
 
 class Test_fit(unittest.TestCase):
     
     def test_minimize3d(self):
         
-        def _model(x,params):
-            a = params[0]
-            b = params[1]
+        def _linear_model(x,a,b):
+            return a * x + b
 
         print('Testing vj.core.minimize3D ...')
-        shape = (100,100,100,10)
+        # set up data
+        shape = (10,10,10,10)
         testarr = np.ones(shape)
         noise = np.random.rand(*shape)
         mult = np.array([float(i) for i in range(1,11)])
-        testarr = testarr * mult + noise
-        # model
+        y3d = testarr * mult + noise
+        x3d = testarr * (mult-1)
         
-        # creating residual
-    
+        # set up lmfit.Parameters
+        model = lmfit.Model(_linear_model)
+        keys = model.param_names
+        vals = 0, 1
+        params_dict = dict(zip(keys, vals))
+        params3d = make_params3d(model, shape[0:3], **params_dict)
+        print(params3d[0,0,0])
+        res3d = fit3d(_linear_model, y3d, params3d, x3d)
 
-        pass
+        #p[lotting results
+        res_choosen = res3d[5,5,5]
+        x = x3d[5,5,5,:]
+        y = y3d[5,5,5,:]
+        plt.plot(x, y, 'bo')
+        plt.plot(x,res_choosen.init_fit, 'k--')    
+        plt.plot(x,res_choosen.best_fit, 'r--')    
+        plt.show()
 
 
--- vnmrjpy/core/fit.py
+++ vnmrjpy/core/fit.py
@@ -1,13 +1,72 @@
 import numpy as np
 from lmfit import minimize, Parameters
+import lmfit
 import matplotlib.pyplot as plt
 import itertools
 import vnmrjpy as vj
 
 """
-Collection of functions aiding the fitting of 3D timeseries data
+Collection of functions aiding the fitting of 3D timeseries data.
+lmfit is the main python module utilized.
 """
+#TODO
+# make these more to the point
+def get_params3d(res3d, model, attribute='best_values'):
+    """Extract fit3d result into a list of 3d volumes of parameters
 
+    Args:
+        res3d -- 3d numpy array of model.ModelResult; output of fit3d
+        model -- lmfit Model of the fit
+    Return:
+        param3d_list -- 
+    """
+
+def make_params3d(model, shape3d, **kwargs):
+    """Wrapper for lmfit.Model.make_params() . Return 3d numpy array of Parameters
+    
+    Args:
+        model -- lmfit.Model instance
+        shape3d -- tuple of lenght 3
+        **kwargs -- **kwargs to put into model.make_params() method 
+    Return:
+        params3d -- 3d numpy array filled with lmfit Parameters instances
+    """
+    param1d = model.make_params(**kwargs)
+    data_type= type(param1d)
+    params3d = np.empty(shape=shape3d,dtype=data_type)
+    params3d.fill(param1d)
+    return params3d
+
+def fit3d(func, y3d, params3d, x3d, method='individual',device='cpu', **kwargs):
+    """Perform model fitting on 3D timeseries data with lmfit Model.fit() method
+
+    For each point in the 3d space an lmfit.Model is created and Model.fit is
+    performed on the timeseries at that point. The model is created 
+
+    Args:
+        func -- callable python function of form f(x, *args, **kwargs)
+        y3d -- 4d numpy.ndarray of y data series (measurement, dims=[x,y,z,val])
+        params3d -- initialized lmfit.Parameters in 3d numpy array
+        x3d -- 4d numpy.ndarray of x data series (grid, dims=[x,y,z,val])
+        **kwargs -- **kwargs going into model.fit() method
+    Return:
+        res3d -- lmfit.model.ModelResult in 3d numpy array
+    """
+
+    (x_dim, y_dim, z_dim) = y3d.shape[0:3]
+    res3d = np.empty((x_dim,y_dim,z_dim),dtype=type(lmfit.model.ModelResult))
+    model = lmfit.Model(func)
+    if device == 'cpu':
+        for x, y, z in itertools.product(*map(range, (x_dim,y_dim,z_dim))):
+            res = model.fit(y3d[x,y,z], params3d[x,y,z], x=x3d[x,y,z], **kwargs)
+            res3d[x,y,z] = res
+        return res3d
+
+    elif device == 'sge':
+        raise(Exception('SGE not supported yet'))
+
+#TODO
+# deprecated
 def residual1D(params, model, x_data, y_data, eps_data):
     """Return residual for least squares fitting
     
@@ -24,6 +83,8 @@ def residual1D(params, model, x_data, y_data, eps_data):
     residual =  (y_data - model_fit) / eps_data
     return residual
 
+#TODO
+#deprecated
 def minimize3D(residual, params3D, args3D, mask3D=None):
     """Wrapper for lmfit.minimize for 3D timeseries
 
@@ -58,11 +119,14 @@ def minimize3D(residual, params3D, args3D, mask3D=None):
             except:
                 pass
     return out3D
-
+#TODO
+# deprecated
 def fit3D_leastsq_2stage():
 
     pass
 
+#TODO
+# deprecated
 def reinit3D():
 
     pass
